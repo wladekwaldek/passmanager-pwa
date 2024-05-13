8422595b0add4ef83c25256bec38772906466233
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2024-05-13 18:26:25 +0200

    bot1

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const express = require("express");
-const path = require("path");
-const http = require("http");
+const TelegramApi = require("node-telegram-bot-api");
 
-const app = express();
+const token = "6557883645:AAEWOZuJRzIms5e56MnSxPCNX0oCLRRgOaA";
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use("/", express.static(path.join(__dirname, "client", "build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+const bot = new TelegramApi(token, { polling: true });
 
-async function start() {
-  try {
-    app.listen(5000, () => {
-      console.log("Server has been started on port" + 5000);
-    });
-  } catch (e) {
-    console.log(e.message);
-    process.exit(1);
-  }
-}
-start();
+bot.on("message", (msg) => {
+  console.log(msg);
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "hi");
+});
